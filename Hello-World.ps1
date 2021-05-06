4923e7a50d371d8724a67e5c649b4b94dabad8b5
Ayan Mullick <ayanmullick@gmail.com>
2021-05-05 21:19:34 -0500

    Update Hello-World.ps1

--- Hello-World.ps1
+++ Hello-World.ps1
@@ -1,1 +1,4 @@
-'Hello World4'
+function Hello-World 
+    {param( [ValidateSet('Green','Red','Blue')] $Color)	#Added parameter vith validateset to test intellisense
+     Write-Host "Hello world!" -Foreground $Color
+     }
