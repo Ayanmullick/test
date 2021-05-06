90db67d485a393adba647412306cc47bd40dacfc
Ayan Mullick <ayanmullick@gmail.com>
2021-05-06 13:34:24 -0500

    Update Hello-World.ps1

--- Hello-World.ps1
+++ Hello-World.ps1
@@ -1,5 +1,2 @@
-function Hello-World 
-    {[CmdletBinding()]
      param( [ValidateSet('Green','Red','Blue')] $Color)	#Added parameter with validateset to test intellisense
      Write-Host "Hello world!" -Foreground $Color
-     }
