87b7622b0e7f65e0c86d131dbc523bc33f4c0ee7
Ayan Mullick <ayanmullick@gmail.com>
2025-08-15 23:26:03 -0500

    Update ubuntuprocesses.ps1

--- PowerShell/ubuntuprocesses.ps1
+++ PowerShell/ubuntuprocesses.ps1
@@ -1,7 +1,10 @@
+$stamp = '{0:MMddyy:HHmmss} {1}' -f ($ct=[TimeZoneInfo]::ConvertTimeFromUtc((Get-Date).ToUniversalTime(),$tz=[TimeZoneInfo]::FindSystemTimeZoneById('America/Chicago'))),($tz.IsDaylightSavingTime($ct)?'CDT':'CST')
+
 $null = New-Item -Path site -ItemType Directory -Force
 
-$stamp = '{0:MMddyy:HHmmss} {1}' -f ($nowCT=Get-Date -TimeZone ($tz=Get-TimeZone -Id 'America/Chicago').Id), ($tz.IsDaylightSavingTime($nowCT)?'CDT':'CST')
-
-Get-Process | Sort-Object -Property WorkingSet64 -Descending | Select-Object -First 20 ProcessName, Id, CPU, @{N = 'WS(MB)'; E = {[math]::Round($_.WorkingSet64/1MB, 2)}} |
+Get-Process |
+  Sort-Object -Property WorkingSet64 -Descending |
+  Select-Object -First 20 ProcessName, Id, CPU,
+    @{Name = 'WS(MB)'; Expression = {[math]::Round($_.WorkingSet64/1MB, 2)}} |
   ConvertTo-Html -As Table -Title 'Processes' -PreContent "<h1>Processes (Ubuntu runner)</h1><p>Updated $stamp</p>" |
   Set-Content site/ubuntuprocesses2.html -Encoding utf8
