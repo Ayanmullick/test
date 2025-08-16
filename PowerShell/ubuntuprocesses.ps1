41383d6303b58a909625b45f7ca4e9f522291680
Ayan Mullick <ayanmullick@gmail.com>
2025-08-16 00:02:54 -0500

    Update ubuntuprocesses.ps1

--- PowerShell/ubuntuprocesses.ps1
+++ PowerShell/ubuntuprocesses.ps1
@@ -1,6 +1,7 @@
 $null = New-Item -Path site -ItemType Directory -Force
 
-$stamp='{0:MMddyy:HHmmss} {1}' -f ($ct = [System.TimeZoneInfo]::ConvertTime([DateTimeOffset]::UtcNow, [System.TimeZoneInfo]::FindSystemTimeZoneById("America/Chicago"))), ([System.TimeZoneInfo]::FindSystemTimeZoneById("America/Chicago").IsDaylightSavingTime($ct.DateTime) ? 'CDT' : 'CST')
+$stamp= '{0:MMddyy:HHmmss} {1}' -f ($ct=[TimeZoneInfo]::ConvertTimeBySystemTimeZoneId([DateTimeOffset]::UtcNow,'America/Chicago')),(($ct.Offset.TotalHours -eq -5)?'CDT':'CST')
+#'{0:MMddyy:HHmmss} {1}' -f ($ct = [System.TimeZoneInfo]::ConvertTime([DateTimeOffset]::UtcNow, [System.TimeZoneInfo]::FindSystemTimeZoneById("America/Chicago"))), ([System.TimeZoneInfo]::FindSystemTimeZoneById("America/Chicago").IsDaylightSavingTime($ct.DateTime) ? 'CDT' : 'CST')
 
 Get-Process |Sort-Object -Property WorkingSet64 -Descending | Select-Object -First 20 ProcessName, Id, CPU, @{N = 'WS(MB)'; E = {[math]::Round($_.WorkingSet64/1MB, 2)}} |
   ConvertTo-Html -As Table -Title 'Processes' -PreContent "<h1>Processes (Ubuntu runner)</h1><p>Updated $stamp</p>" |
