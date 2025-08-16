e835fe10e504e6b4e2da0eed6ea47d5036d068c7
Ayan Mullick <ayanmullick@gmail.com>
2025-08-15 23:55:00 -0500

    Update ubuntuprocesses.ps1

--- PowerShell/ubuntuprocesses.ps1
+++ PowerShell/ubuntuprocesses.ps1
@@ -1,12 +1,7 @@
-$tz=[TimeZoneInfo]::FindSystemTimeZoneById('America/Chicago')
-$ct=[TimeZoneInfo]::ConvertTimeFromUtc([DateTime]::UtcNow,$tz)
-$stamp='{0:MMddyy:HHmmss} {1}'-f$ct,($tz.IsDaylightSavingTime($ct)?'CDT':'CST')
-
 $null = New-Item -Path site -ItemType Directory -Force
 
-Get-Process |
-  Sort-Object -Property WorkingSet64 -Descending |
-  Select-Object -First 20 ProcessName, Id, CPU,
-    @{Name = 'WS(MB)'; Expression = {[math]::Round($_.WorkingSet64/1MB, 2)}} |
+$stamp='{0:MMddyy:HHmmss} {1}' -f ($ct = [System.TimeZoneInfo]::ConvertTime([DateTimeOffset]::UtcNow, [System.TimeZoneInfo]::FindSystemTimeZoneById("America/Chicago"))), ([System.TimeZoneInfo]::FindSystemTimeZoneById("America/Chicago").IsDaylightSavingTime($ct.DateTime) ? 'CDT' : 'CST')
+
+Get-Process |Sort-Object -Property WorkingSet64 -Descending | Select-Object -First 20 ProcessName, Id, CPU, @{N = 'WS(MB)'; E = {[math]::Round($_.WorkingSet64/1MB, 2)}} |
   ConvertTo-Html -As Table -Title 'Processes' -PreContent "<h1>Processes (Ubuntu runner)</h1><p>Updated $stamp</p>" |
   Set-Content site/ubuntuprocesses2.html -Encoding utf8
