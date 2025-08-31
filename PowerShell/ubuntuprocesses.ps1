7b0e208213c62b0304358b2e59ac45252296fa40
Ayan Mullick <ayanmullick@gmail.com>
2025-08-30 20:34:22 -0500

    .

--- PowerShell/ubuntuprocesses.ps1
+++
@@ -1,7 +0,0 @@
-$null = New-Item -Path site -ItemType Directory -Force
-
-$stamp= '{0:MMddyy:HHmmss} {1}' -f ($ct=[TimeZoneInfo]::ConvertTimeBySystemTimeZoneId([DateTimeOffset]::UtcNow,'America/Chicago')),(($ct.Offset.TotalHours -eq -5)?'CDT':'CST')
-
-Get-Process |Sort-Object -Property WorkingSet64 -Descending | Select-Object -First 20 ProcessName, Id, CPU, @{N = 'WS(MB)'; E = {[math]::Round($_.WorkingSet64/1MB, 2)}} |
-  ConvertTo-Html -As Table -Title 'Processes' -PreContent "<h1>Processes (Ubuntu runner)</h1><p>Updated $stamp</p>" |
-  Set-Content site/ubuntuprocesses2.html -Encoding utf8
